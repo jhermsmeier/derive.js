2d8ccb314a8fbfc4def94a39511010c966176860
jhermsmeier <jhermsmeier@googlemail.com>
2013-10-16 17:50:17 +1300

    Updated derive.js: Fixed multiple inheritance
    derive() now properly inherits from prototype's prototypes,
    meaning you can do things like derive( Ctor, HTMLElement )
    and Ctor's proto will get the full chain (HTMLElement > Element > Node >
    EventTarget > Object) functionality.
    
    The annoying drawback of this technique is, that `instanceof` checks for
    inherited prototypes will fail (return false). But hey, at least we
    inherit the full functionality.
    
    derive( Ctor, Super1, Super2, Super3, ... ) also does work properly now.

--- derive.js
+++ derive.js
@@ -1,29 +1,84 @@
+
+if( typeof module !== 'undefined' ) {
+  module.exports = derive
+}
+
 /**
  * Prototype inheriter capable of
  * nearly everything you ever wanted
  * @param  {Function} constructors..
  * @return {Object}   prototype
  */
-function derive( ctor /*, supers.. */ ){
+function derive( target /*, supers.. */ ) {
   
-  var supers = [].slice.call( arguments )
+  var ctors = Array.prototype.slice
+    .call( arguments )
   
-  supers.push( Object.prototype )
-  supers.reverse()
+  var prototypes = Array.prototype.concat
+    .apply( [], derive.tree( ctors ) )
+    .reverse()
   
-  return ctor.prototype = supers.reduce( function( proto, ctor ) {
-    return Object.create( proto,
-      Object.getOwnPropertyNames( ctor.prototype )
-        .reduce( function( desc, key ) {
-            desc[ key ] = Object.getOwnPropertyDescriptor( ctor.prototype, key )
-            return desc
-          }, Object.create( null )
-        )
-    )
+  return target.prototype = prototypes.reduce( function( current, next ) {
+    
+    var description = Object.getOwnPropertyNames( next )
+      .reduce( function( desc, key ) {
+        desc[ key ] = Object.getOwnPropertyDescriptor( next, key )
+        return desc
+      }, Object.create( null ))
+    
+    return Object.create( current, description )
+    
   })
   
 }
 
-if( typeof module !== 'undefined' ) {
-  module.exports = derive
+/**
+ * Gets a prototype chain tree
+ * @param  {Array} ctors
+ * @return {Array} tree
+ */
+derive.tree = function( ctors ) {
+  
+  var i = 0, tree = []
+  var chain, len = ctors.length
+  
+  for( i = 0; i < len; i++ ) {
+    // Get the prototype chain of each constructor
+    if( chain = derive.chain( ctors[i] ) ) {
+      // Remove trailing Object.prototype,
+      if( chain.length > 1 && i < len - 1 ) {
+        // Only remove if it's not the only
+        // prototype in the chain (=> an inheritance of Object)
+        if( chain[ chain.length - 1 ] === Object.prototype ) {
+          chain.length--
+        }
+      }
+      // Append to prototype chain tree
+      tree.push( chain )
+    }
+  }
+  
+  return tree
+  
+}
+
+/**
+ * Retrieves the prototype chain of an object
+ * @param  {Object} object
+ * @return {Array}  prototypes
+ */
+derive.chain = function( object ) {
+  
+  var prototypes = []
+  var proto = Object.create( object.prototype )
+  
+  while( proto = Object.getPrototypeOf( proto ) ) {
+    if( !~prototypes.indexOf( proto ) ) {
+      prototypes.push( proto )
+    }
+  }
+  
+  return prototypes.length ?
+    prototypes : undefined
+  
 }
